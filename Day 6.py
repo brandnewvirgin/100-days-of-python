0d418f5b21c1a8e2951210b47269a78966c4fd28
Ochuko Atanega <77187875+brandnewvirgin@users.noreply.github.com>
2025-08-08 16:56:32 +0100

    Add files via upload

---
+++ Day 6.py
@@ -0,0 +1,207 @@
+# print()
+# num_char = len("Hello")
+# print(num_char)
+#
+# #creating a function
+# def my_function(): #define function
+#     print("Hello") #call the function
+#     print("Bye")
+#
+# my_function()
+#
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# turn_left()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
+
+#Reeborg's world robot flag game
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# for right in turn_right():
+#     turn_left()
+#     move()
+#     turn_right()
+# else:
+#     turn_right()
+#
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+
+# # define a new function named "jump"
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+#
+# jump()
+# jump()
+# jump()
+# jump()
+# jump()
+# jump()
+
+# #for loop in newly defined functions
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# # define a new function named "jump"
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+#
+# for step in range(6):
+#     jump()
+#
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         done()
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+# while not at_goal():
+#     if right_is_clear()
+#         turn_right()
+#         while wall_on_right():
+#             turn_left()
+#             if front_is_clear():
+#                 move()
+#             elif wall_in_front():
+#                 jump()
+#             elif wall_on_right():
+#                 turn_left()
+#             elif right_is_clear():
+#                 turn_right()
+#             else:
+#                 move()
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#      turn_left()
+#     while not at_goal():
+#     jump()
+#     else:
+#         move()
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def jump():
+#     turn_left()
+#
+#     while wall_on_right():
+#         move()
+#
+#     turn_right()
+#     move()
+#     turn_right()
+#
+#     while front_is_clear():
+#         move()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
